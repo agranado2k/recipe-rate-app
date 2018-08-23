d3ef64047d9098f36c599c4bf1a885242b5e0f38
Arthur Granado <agranado2k@gmail.com>
2018-08-23 23:30:29 +1000

    Paginate the resource GET /v1/recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,10 @@
 class RecipesController < ApplicationController
   def index
-    recipes = Recipe.all
+    limit = params[:limit].present? ? params[:limit].to_i : 30
+    page = params[:page].present? ? params[:page].to_i : 1
+
+    offset = (page- 1) * limit
+    recipes = Recipe.limit(limit).offset(offset)
 
     render json: recipes, status: :ok
   end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -2,21 +2,42 @@ require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
   context 'GET /v1/recipes' do
+    let(:page) { nil }
+    let(:limit) { nil }
+    let(:params) { { page: page, limit: limit } }
     before do
-      create(:recipe)
-      create(:recipe)
+      7.times { create(:recipe) }
 
-      get :index
+      get :index, params: params
     end
 
     it 'returns HTTP statsu 200' do
       expect(response).to have_http_status 200
     end
 
-    it 'returns all users' do
+    it 'return users' do
       body = JSON.parse(response.body)
 
-      expect(body.size).to eq(2)
+      expect(body.size).to eq(7)
+    end
+
+    context 'returns all users' do
+      let(:limit) { 5 }
+
+      it 'return users page 1 (default)' do
+        body = JSON.parse(response.body)
+
+        expect(body.size).to eq(5)
+      end
+
+      context 'page 2' do
+        let (:page) { 2 }
+        it 'return users from page 2 ' do
+          body = JSON.parse(response.body)
+
+          expect(body.size).to eq(2)
+        end
+      end
     end
   end
 end
